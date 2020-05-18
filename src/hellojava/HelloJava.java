9fd69932a6ebc1aaf7540f37ce221096013b03bc
alinefbrito <alinefbrito@gmail.com>
2020-05-18 14:18:35 -0300

    Alteração no texto do Hello Java

--- src/hellojava/HelloJava.java
+++ src/hellojava/HelloJava.java
@@ -15,7 +15,7 @@ public class HelloJava {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hello Java!");
+        System.out.println("Hello Java :D!");
     }
-    
+
 }
